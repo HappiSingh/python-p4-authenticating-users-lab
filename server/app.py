241c942bc20b931af44047910ceb46f8b9935f0e
Harpreet Singh <Harpreets1201@gmail.com>
2023-09-15 23:38:17 -0400

    Completed lab

--- server/app.py
+++ server/app.py
@@ -48,6 +48,38 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+
+
+class Login(Resource):
+
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        session['user_id'] = user.id
+
+        user_json = jsonify(user.to_dict())
+        return make_response(user_json, 200)
+    
+
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+    
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session['user_id']
+        if user_id:
+           user = User.query.filter(User.id == user_id).first()
+           user_dict = jsonify(user.to_dict())
+           return make_response(user_dict, 200)
+        return {}, 401
+        
+
+api.add_resource(CheckSession,'/check_session')
+api.add_resource(Logout, '/logout')
+api.add_resource(Login, '/login')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
